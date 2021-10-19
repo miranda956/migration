bfa318d0586b42dbaefb4bd45876a52bb13c37b5
miranda956 <musularonald17@gmail.com>
2021-10-20 00:41:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// require csvtojson module
+const CSVToJSON = require('csvtojson');
+
+// convert users.csv file to JSON array
+CSVToJSON().fromFile('data.csv')
+    .then(users => {
+        // users is a JSON array
+        // log the JSON array
+        console.log(users);
+    }).catch(err => {
+        // log error if any
+        console.log(err);
+    });
